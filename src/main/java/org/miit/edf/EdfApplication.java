a5f3f45fe9408b8dfbc9b54a9fdab5dc2b25d870
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2024-04-11 18:13:22 +0300

    initial commit

---
+++ src/main/java/org/miit/edf/EdfApplication.java
@@ -0,0 +1,13 @@
+package org.miit.edf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/miit/edf/EdfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miit.edf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
